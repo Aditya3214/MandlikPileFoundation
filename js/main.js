98607a1165d8deb0c153f11069a0e0bc2e5d423e
Aditya Jagtap <aditya.jagtap@indionetworks.com>
2025-10-23 07:54:57 +0530

    Mandlik Pile Foundation Projects Files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// Modern scroll animation
+document.addEventListener("DOMContentLoaded", function() {
+    const animatedElements = document.querySelectorAll('.animate-on-scroll');
+
+    if (!animatedElements) {
+        return;
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1 // Trigger when 10% of the element is visible
+    });
+
+    animatedElements.forEach(element => {
+        observer.observe(element);
+    });
+});
